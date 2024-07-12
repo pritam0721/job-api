ec386724de37242ac49dc5fb5dfb809c388ca04a
pritam0721 <pritamgayen629@gmail.com>
2024-07-12 23:59:38 +0530

    issue resoslve with new collection and user.int()

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,28 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
-const bcrypt = require("bcryptjs");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
+  User.init(); //
 
-  const salt = await bcrypt.genSalt(10);
-  const hashPassword = await bcrypt.hash(password,salt);
+  const user = await User.create({ ...req.body });
 
-  const tempUser = {name , email ,password:hashPassword}
-  console.log(email);
+  const token = user.createJWT();
 
-  const user = await User.create({ ...tempUser });
-  res.status(StatusCodes.CREATED).json({ user });
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
-  res.send("user login");
+  const { email,password } = req.body;
+  if (!password || !email) {
+    throw new BadRequestError("please give email and password ");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("please provide a valid email ");
+  }
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+const  jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -10,13 +11,14 @@ const UserSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: [true , "Please provide email"],
+    unique: true,
+    required: [true, "Please provide email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide a valid email",
     ],
-    unique: true,
   },
+
   password: {
     type: String,
     required: [true, "Please provide password"],
@@ -24,4 +26,22 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('User',UserSchema)
+
+
+UserSchema.pre('save',async function(){
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password,salt)
+  
+})
+
+UserSchema.methods.createJWT  = function(){
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+}
+
+
+
+module.exports = mongoose.model("User", UserSchema);
